d546845f91a3503257a3b0c146e5dab859ffe360
VGabriel45 <56271768+VGabriel45@users.noreply.github.com>
2020-12-14 11:06:47 +0200

    initialized spring boot app

---
+++ src/test/java/com/jobifyProject/jobify/JobifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobifyProject.jobify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
