b93795b542bcc413366bc96e60905a49486e3185
VGabriel45 <gabriel_marian22@yahoo.com>
2020-12-15 16:30:19 +0200

    added spring security configuration for form based auth

--- src/main/java/com/jobifyProject/jobify/JobifyProjectApplication.java
+++ src/main/java/com/jobifyProject/jobify/JobifyProjectApplication.java
@@ -2,6 +2,7 @@ package com.jobifyProject.jobify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
