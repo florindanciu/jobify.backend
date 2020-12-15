b8cffff05dcf7d587b2c0387aab107d3d3658d0e
VGabriel45 <gabriel_marian22@yahoo.com>
2020-12-15 14:45:53 +0200

    added form based configuration and first mock user for testing

--- src/main/java/com/jobifyProject/jobify/JobifyProjectApplication.java
+++ src/main/java/com/jobifyProject/jobify/JobifyProjectApplication.java
@@ -20,25 +20,25 @@ public class JobifyProjectApplication {
 		SpringApplication.run(JobifyProjectApplication.class, args);
 	}
 
-	@Bean
-	public Docket swaggerConfig(){
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.paths(PathSelectors.ant("/api/*"))
-				.apis(RequestHandlerSelectors.basePackage("com.jobify.jobifyProject"))
-				.build()
-				.apiInfo(apiDetails());
-	}
-
-	private ApiInfo apiDetails(){
-		return new ApiInfo(
-				"Jobify API",
-				"This is the official API documentation of Jobify.",
-				"1.0",
-				"Free to use",
-				new springfox.documentation.service.Contact("Vasile Gabriel", "https://www.linkedin.com/in/vasile-gabriel-marian-11155019a/", "gabriel_marian22@yahoo.com"),
-				"API License",
-				"google.com",
-				Collections.emptyList());
-	}
+//	@Bean
+//	public Docket swaggerConfig(){
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.paths(PathSelectors.ant("/api/*"))
+//				.apis(RequestHandlerSelectors.basePackage("com.jobifyProject.jobify"))
+//				.build()
+//				.apiInfo(apiDetails());
+//	}
+//
+//	private ApiInfo apiDetails(){
+//		return new ApiInfo(
+//				"Jobify API",
+//				"This is the official API documentation of Jobify.",
+//				"1.0",
+//				"Free to use",
+//				new springfox.documentation.service.Contact("Vasile Gabriel", "https://www.linkedin.com/in/vasile-gabriel-marian-11155019a/", "gabriel_marian22@yahoo.com"),
+//				"API License",
+//				"google.com",
+//				Collections.emptyList());
+//	}
 }
