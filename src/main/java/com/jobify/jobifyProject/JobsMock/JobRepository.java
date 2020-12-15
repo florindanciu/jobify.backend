3cc24db837f87b3a3d455fd4c071df7a6648e77e
VGabriel45 <gabriel_marian22@yahoo.com>
2020-12-15 13:38:56 +0200

    deleted mock classes

--- src/main/java/com/jobify/jobifyProject/JobsMock/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobify.jobifyProject.JobsMock;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface JobRepository extends CrudRepository<Job, String> {
-}
